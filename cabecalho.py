70f17973625a8af27a7eb06c156bffca048b2586
RafaelKauffmann <168317046+RafaelKauffmann@users.noreply.github.com>
2024-04-30 20:09:20 -0300

    Add files via upload

---
+++ cabecalho.py
@@ -0,0 +1,16 @@
+def main():
+    # head
+    print(' ================== x_x ========================== x_x ==================')
+    print('|                                                                        |')
+    print('| Bem-vindo ao SUPER BATALHA NAVAL HUMBERTO REDUX LIMITED DELUXE EDITION |')
+    print('|                                                                        |')
+    print(' ====================== ( °ㅁ°) ========== (°ㅁ° ) ======================\n')
+
+    # memes e aviso ao jogador
+    t.sleep(1)
+    print('Lembre-se de jogar em tela cheia para maior Humbersã... quer dizer, imersão!')
+
+    # seletor de dificuldade e escolha do país de Humberto
+    print('Dificuldades:')
+    print('"Tá fácil a vida??" [1] / "Normal... sem graça..." [2] / "Vejo vocês no quiz de quarta-feira..." [3]')
+    dif = int(input('Selecione a dificuldade: '))
